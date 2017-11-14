f10ddc8c34764b57ac72e09dcc2a2d07b228eb9d
toelapiut <toelapiut7@gmail.com>
2017-11-14 10:15:42 +0300

    added models to connect to the database

---
+++ news/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+
+class Editor(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email=models.EmailField()
+    
+    def __str__(self):
+        return self.first_name 
+
+class tags(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title=models.CharField(max_length=60)
+    post=models.TextField()
+    editor=models.ForeignKey(Editor)
+    tags=models.ManyToManyField(tags)
+    pub_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
